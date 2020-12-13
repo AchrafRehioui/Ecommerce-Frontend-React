a5f9273b4ba3520f1671efee70e1cfb668890432
Achraf <achraf.rehioui.1@gmail.com>
2020-12-13 15:33:17 +0100

    Handle error with toastr and adding SignIn form

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './../core/Layout';
+import toastr from 'toastr';
+import "toastr/build/toastr.css";
+import { API_URL } from './../config';
 
-const Signin = () => {
+const Signin = (props) => {
+
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = e => {
+        setUser({ ...user, [e.target.id]: e.target.value })
+    }
+
+    const submitSignin = e => {
+        e.preventDefault();
+
+        fetch(`${API_URL}/signin`, {
+            method: "POST",
+            headers: {
+
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    toastr.warning(res.error, 'Please Check form !', {
+                        positionClass: "toast-bottom-left",
+                    })
+                }
+                else {
+                    toastr.info('User is authenticated SuccessFully', 'welcome', {
+                        positionClass: "toast-bottom-left",
+                    })
+
+                    props.history.push('/home')
+                }
+
+
+
+            })
+            .catch(err => toastr.error(err, 'Server error !', {
+                positionClass: "toast-bottom-left",
+            }))
+    }
+
+    const form = () => (
+
+        <form onSubmit={submitSignin}>
+
+            <div className="form-group">
+                <label htmlFor="email" className="text-muted">email</label>
+                <input onChange={handleChange} type="email" className="form-control" id="email" />
+            </div>
+
+
+            <div className="form-group">
+                <label htmlFor="password" className="text-muted">password</label>
+                <input onChange={handleChange} type="password" className="form-control" id="password" />
+            </div>
+
+            <button className="btn btn-lg btn-block btn-outline-info">Sign In</button>
+        </form>
+
+    )
+
     return (
         <div>
             <Layout
@@ -9,10 +79,15 @@ const Signin = () => {
                 description="Sign In Node React Ecommerce App"
                 className="container"
             >
-                <h2>SignIn</h2>
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        {form()}
+                    </div>
+                </div>
+
             </Layout>
         </div>
     )
 }
 
-export default Signin
+export default Signin;
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Layout from './../core/Layout';
+import toastr from 'toastr';
+import "toastr/build/toastr.css";
+import { API_URL } from './../config';
 
-const Signup = () => {
+const Signup = (props) => {
 
 
     const [user, setUser ] = useState({
@@ -11,13 +14,48 @@ const Signup = () => {
     })
 
     const handleChange = e => {
-
         setUser({...user, [e.target.id]: e.target.value})
+    }
+
+    const submitSignup = e => {
+        e.preventDefault();
+
+        fetch(`${API_URL}/signup`, {
+            method: "POST",
+            headers: {
+
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(res.error) {
+                toastr.warning(res.error, 'Please Check form !', {
+                    positionClass: "toast-bottom-left",
+                })
+            }
+            else {
+                toastr.success('User is created SuccessFully', 'New Accout', {
+                    positionClass: "toast-bottom-left",
+                })
+
+                props.history.push('/signin')
+            }
+
+            
+
+        })
+        .catch(err =>  toastr.error(err, 'Server error !', {
+            positionClass: "toast-bottom-left",
+        }))
     }
 
     const form = () => (
 
-        <div> 
+        <form onSubmit={submitSignup}> 
             <div className="form-group">
                 <label htmlFor="name" className="text-muted">name</label>
                 <input onChange={handleChange} type="text" className="form-control" id="name" />
@@ -36,7 +74,7 @@ const Signup = () => {
 
             <button className="btn btn-lg btn-block btn-outline-success">Sign Up</button>
             { JSON.stringify(user)}
-        </div>
+        </form>
         
     )
 
