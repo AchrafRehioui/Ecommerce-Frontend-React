beb2db017819990501f34e361285500269f5c932
Achraf <achraf.rehioui.1@gmail.com>
2020-12-13 13:51:55 +0100

    adding Sign Up Form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,6 +2,30 @@ import React from 'react';
 import Layout from './../core/Layout';
 
 const Signup = () => {
+
+    const form = () => (
+
+        <div> 
+            <div className="form-group">
+                <label htmlFor="name" className="text-muted">name</label>
+                <input type="text" className="form-control" id="name" />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="email" className="text-muted">email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+
+
+            <div className="form-group">
+                <label htmlFor="password" className="text-muted">password</label>
+                <input type="password" className="form-control" id="password"/>
+            </div>
+
+            <button className="btn btn-lg btn-block btn-outline-success">Sign Up</button>
+        </div>
+    )
+
     return (
         <div>
             <Layout
@@ -9,7 +33,12 @@ const Signup = () => {
                 description="Sign Up Node React Ecommerce App"
                 className="container"
             >
-                <h2>SignUp</h2>
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        { form() }
+                    </div>
+                </div>
+                
             </Layout>
         </div>
     )
