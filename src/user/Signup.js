17a072138ee4bfe49f3e55d1ede806b016cfa590
Achraf <achraf.rehioui.1@gmail.com>
2020-12-13 14:04:21 +0100

    adding useState hooks for signUp

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './../core/Layout';
 
 const Signup = () => {
+
+
+    const [user, setUser ] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = e => {
+
+        setUser({...user, [e.target.id]: e.target.value})
+    }
 
     const form = () => (
 
         <div> 
             <div className="form-group">
                 <label htmlFor="name" className="text-muted">name</label>
-                <input type="text" className="form-control" id="name" />
+                <input onChange={handleChange} type="text" className="form-control" id="name" />
             </div>
 
             <div className="form-group">
                 <label htmlFor="email" className="text-muted">email</label>
-                <input type="email" className="form-control" id="email" />
+                <input onChange={handleChange} type="email" className="form-control" id="email" />
             </div>
 
 
             <div className="form-group">
                 <label htmlFor="password" className="text-muted">password</label>
-                <input type="password" className="form-control" id="password"/>
+                <input onChange={handleChange} type="password" className="form-control" id="password"/>
             </div>
 
             <button className="btn btn-lg btn-block btn-outline-success">Sign Up</button>
+            { JSON.stringify(user)}
         </div>
+        
     )
 
     return (
