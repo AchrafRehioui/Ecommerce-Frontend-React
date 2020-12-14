f19e28655333ec9f229a46833af2c34c4c115ca3
Achraf <achraf.rehioui.1@gmail.com>
2020-12-14 17:34:45 +0100

    Adding Dashboard User

--- src/user/Dashboard.js
+++ src/user/Dashboard.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from 'react';
 import Layout from './../core/Layout';
-
+import { isAuthenticated } from './../auth/helpers';
 
 function Dashboard() {
+
+    const { user: { name, email, role } } = isAuthenticated() 
+
+
     return (
         <Fragment>
             <Layout
@@ -10,6 +14,32 @@ function Dashboard() {
                 description="Dashboard user"
                 className="container"
             >
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">User INFORMATION</h5>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">{name}</li>
+                                    <li className="list-group-item">{email}</li>
+                                    <li className="list-group-item">{role ? 'Admin' : 'User'}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Purchase HISTORY</h5>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </Layout>
         </Fragment>
