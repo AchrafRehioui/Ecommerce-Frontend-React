141f2de7878af147114b308d6e1e7448f4b8282c
Achraf <achraf.rehioui.1@gmail.com>
2020-12-16 12:25:05 +0100

    showing firstly Products by arrivals and sell in Hompe page

---
+++ src/core/ApiCore.js
@@ -0,0 +1,12 @@
+
+import { API_URL } from './../config';
+
+export const getProducts = (sortBy, order, limit) => {
+
+
+    return fetch(`${API_URL}/product?${sortBy}&order=${order}&limit=${limit}`)
+        .then(res => res.json())
+        .then(res => res.products)
+        .catch(err => console.error(err))
+
+}
--- src/core/home.js
+++ src/core/home.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './ApiCore';
 
 
-const home = () => {
+function Home() {
+
+    const [productsBestSellers, setProductsBestSellers] = useState([])
+    const [productsArrivals, setProductsArrivals] = useState([])
+
+    const loadBestSellers = () => {
+
+        getProducts('sold', 'desc', 6)
+            .then(products => setProductsBestSellers(products))
+
+    }
+
+    const loadArrivals = () => {
+
+        getProducts('createdAt', 'desc', 3)
+            .then(products => setProductsArrivals(products))
+
+    }
+
+    useEffect(() => {
+        loadArrivals()
+        loadBestSellers()
+
+    }, [])
+
+
     return (
         <div>
             <Layout
@@ -10,10 +36,15 @@ const home = () => {
                 description="Node React Ecommerce App"
                 className="container"
             >
-                <h2>Hello</h2>
+                <h1>Arrival Products</h1>
+                {JSON.stringify(productsArrivals)}
+                <hr />
+
+                <h1>Best Sellers</h1>
+                {JSON.stringify(productsBestSellers)}
             </Layout>
         </div>
     )
 }
 
-export default home
+export default Home;
