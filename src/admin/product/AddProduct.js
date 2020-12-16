fd17ee6dc2eb3567cc3711a73d23f89c672ac4b8
Achraf <achraf.rehioui.1@gmail.com>
2020-12-16 10:56:22 +0100

    Persist Product with image

--- src/admin/product/AddProduct.js
+++ src/admin/product/AddProduct.js
@@ -7,6 +7,8 @@ import { isAuthenticated } from './../../auth/helpers';
 
 function AddProduct() {
 
+    const { user, token } = isAuthenticated();
+
     const [product, setProduct] = useState({
         photo: '',
         name: '',
@@ -15,59 +17,64 @@ function AddProduct() {
         price: 0,
         category: 0,
         shipping: false
-    });
+    })
+
+    const [formData, setFormData] = useState(new FormData()); 
 
     const handleChange = (e) => {
 
-        setProduct({...product, [e.target.id]: value});
+        const value = e.target.id === 'photo' ? e.target.files[0] : e.target.value;
 
+        formData.set(e.target.id, value)
 
-    }
+        setProduct({...product, [e.target.id]: value})
+        
+     }
 
-    const submitProduct = (e) => {
-
+const submitProduct = (e) => {
+        
         e.preventDefault();
-
-        const { user, token } = isAuthenticated();
 
         fetch(`${API_URL}/product/create/${user._id}`, {
             method: "POST",
             headers: {
-
                 "Accept": "application/json",
-                "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}`
-
             },
-            body: JSON.stringify({ product })
+            body: formData
         })
-            .then(res => res.json())
-            .then(res => {
-                if (res.error) {
-                    toastr.warning(res.error, 'Please Check form !', {
-                        positionClass: "toast-bottom-left",
-                    })
-                }
-                else {
-                    toastr.success(`Product ${product.name} created`, 'new new Product', {
-                        positionClass: "toast-bottom-left",
-                    })
+        .then(res => res.json())
+        .then(res => {
+            if(res.error) {
+                toastr.warning(res.error, 'Please Check form !', {
+                    positionClass: "toast-bottom-left",
+                })
+            }
+            else {
+                toastr.success(`Product ${product.name} created`, 'new Product', {
+                    positionClass: "toast-bottom-left",
+                })
 
-                    setProduct({
-                        photo: '',
-                        name: '',
-                        description: '',
-                        quantity: 0,
-                        price: 0,
-                        category: 0,
-                        shipping: false
-                    })
-                }
-            })
-            .catch(err => toastr.error(err, 'Server error !', {
-                positionClass: "toast-bottom-left",
-            }))
-    }
+                setProduct({
+                    photo: '',
+                    name: '',
+                    description: '',
+                    quantity: 0,
+                    price: 0,
+                    shipping: false,
+                    category: 0
+                })
+
+                setFormData(new FormData())
+
+            }
+
+        })
+        .catch(err =>  toastr.error(err, 'Server error !', {
+                    positionClass: "toast-bottom-left",
+                }))
+  
+     }
 
     return (
         <div>
@@ -77,47 +84,49 @@ function AddProduct() {
                 className="container"
             >
                 <div className="row">
-                    <div className="col-md-6 mx-auto">
-                        <form onSubmit={submitProduct}>
+                   <div className="col-md-6 mx-auto">
+                       <form onSubmit={submitProduct}>
+
+                        <div className="form-group">
+                            <label htmlFor="photo">Photo product</label>
+                            <input onChange={handleChange} id="photo" type="file" className="form-control-file" name="photo"  />
+                        </div>
+
+                            <div className="form-group">
+                                <label htmlFor="" className="text-muted">name</label>
+                                <input value={product.name} onChange={handleChange} id="name" required autoFocus placeholder="Add name of Product" type="text" className="form-control"/>
+                            </div>
                             
-                            <div className="input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text">Upload photo</span>
-                                </div>
-                                <div className="custom-file">
-                                    <input type="file" className="cutom-file-input" name="photo"/>
-                                    <label htmlFor="" className="cutom-fole-label">Product</label>
-                                </div>
+                            <div className="form-group">
+                                <label htmlFor="description">description</label>
+                                <textarea value={product.description} onChange={handleChange} id="description" name="description" rows="2" className="form-control"></textarea>
                             </div>
 
                             <div className="form-group">
-                                <label htmlFor="" className="text-muted"></label>
-                                <input  required autoFocus placeholder="Add a Product" onChange={handleChange} type="text" className="form-control" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="description">description</label>
-                                <textarea name="description" id="description" cols="30" rows="10" className="form-control">
-                                </textarea>
-                            </div>
-                            <div className="form-group">
                                 <label htmlFor="quantity">quantity</label>
-                                <input type="number" id="quantity" className="form-control"/>
+                                <input value={product.quantity} onChange={handleChange} type="number" id="quantity" className="form-control"/>
                             </div>
+
                             <div className="form-group">
                                 <label htmlFor="price">price</label>
-                                <input type="number" id="price" className="form-control"/>
+                                <input value={product.price} onChange={handleChange} type="number" id="price" className="form-control"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="category">category</label>
-                                <select name="category" id="category" className="form-control">
-                                    <option value=""></option>
+                                <select value={product.category} onChange={handleChange} name="category" id="category" className="form-control">
+                                    <option value="0">Select a category</option>
+                                    <option value="5fd3937d6f3efd69901abf81">web developpement</option>
                                 </select>
                             </div>
 
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="shipping"/>
-                                <label className="form-check-label" for="shipping">shipping</label>
+                            <div className="form-group">
+                            <label htmlFor="shipping">shipping</label>
+                                <select value={product.shipping} onChange={handleChange} name="shipping" id="shipping" className="form-control">
+                                    <option value="false">No</option>
+                                    <option value="true">Yes</option>
+                                </select>
                             </div>
+                            { JSON.stringify(product) }
 
                             <button className="my-5 btn-block btn btn-outline-primary">New Product</button>
                         </form>
