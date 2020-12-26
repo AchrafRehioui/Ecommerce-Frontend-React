5f5083aa97c9b3ea0c3139324d78f5135b693d93
Achraf <achraf.rehioui.1@gmail.com>
2020-12-27 00:40:51 +0100

    adding Product details for each Order

--- src/admin/order/ListOrders.js
+++ src/admin/order/ListOrders.js
@@ -38,17 +38,45 @@ function ListOrders() {
         }
     }
 
+    const showInput = (key, value) => {
+        return (
+           <div className="form-group my-2">
+               <label htmlFor={key}>{key}</label>
+               <input id={key} value={value} readOnly type="text" className="form-control"/>
+            </div>
+        )
+    } 
+
     const showOrders = () => {
         return orders.length && orders.map(order => (
             <div className="my-3" key={order._id}>
                 <ul class="list-group">
-                    <li class="list-group-item active"><strong>Transact ID</strong>{order.transaction_id}</li>
-                    <li class="list-group-item"><strong>Amount</strong> ${order.amount}</li>
-                    <li class="list-group-item"><strong>Status</strong> {order.status}</li>
-                    <li class="list-group-item"><strong>Ordered On </strong> {moment(order.createdAt).fromNow()}</li>
-                    <li class="list-group-item"><strong>Customer </strong> {order.user.name}</li>
-                    <li class="list-group-item"><strong>Delivery Address </strong> {order.address}</li>
+                    <li className="list-group-item active"><strong>Transact ID</strong>{order.transaction_id}</li>
+                    <li className="list-group-item"><strong>Amount</strong> ${order.amount}</li>
+                    <li className="list-group-item"><strong>Status</strong> {order.status}</li>
+                    <li className="list-group-item"><strong>Ordered On </strong> {moment(order.createdAt).fromNow()}</li>
+                    <li className="list-group-item"><strong>Customer </strong> {order.user.name}</li>
+                    <li className="list-group-item"><strong>Delivery Address </strong> {order.address}</li>
                 </ul>
+                <div className="my-5">
+
+                    <h3 className="display-4 my-3">Total Products {order.products.length}</h3>
+
+                    { order.products.map(product => (
+                    <div key={product._id} class="card text-white bg-secondary mb-3" >
+                        <div className="card-header">{ product.name}</div>
+                        <div className="card-body">
+                        
+                        { showInput('Product ID', product._id) }
+                        { showInput('Product Name', product.name) }
+                        { showInput('Product Price', product.price) }
+                        { showInput('Product quantity', product.count) }
+
+                    </div>
+                  </div>
+                    ))}
+                
+                </div>
             </div>
             ))
     }
