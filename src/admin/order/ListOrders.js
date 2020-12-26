687f87ebb012c41a85d357c30cbd1107eb0a49c7
Achraf <achraf.rehioui.1@gmail.com>
2020-12-26 17:14:04 +0100

    looping all Orders

--- src/admin/order/ListOrders.js
+++ src/admin/order/ListOrders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import { isAuthenticated } from './../../auth/helpers';
 import Layout from './../../core/Layout';
 import { listOfOrders } from './../ApiAdmin';
@@ -21,6 +22,37 @@ function ListOrders() {
         loadOrders(user, token)
     }, []);
 
+    const notOrders = () => {
+        if(orders.length === 0){
+            return (
+                <div className="alert alert-warning text-center my-5 display-3">
+                    Not Orders Yet !
+                </div>
+            )
+        }else {
+            return(
+                <div className="alert alert-info text-center my-5 display-3">
+                    Total Orders {orders.length}
+                </div>
+            )
+        }
+    }
+
+    const showOrders = () => {
+        return orders.length && orders.map(order => (
+            <div className="my-3" key={order._id}>
+                <ul class="list-group">
+                    <li class="list-group-item active"><strong>Transact ID</strong>{order.transaction_id}</li>
+                    <li class="list-group-item"><strong>Amount</strong> ${order.amount}</li>
+                    <li class="list-group-item"><strong>Status</strong> {order.status}</li>
+                    <li class="list-group-item"><strong>Ordered On </strong> {moment(order.createdAt).fromNow()}</li>
+                    <li class="list-group-item"><strong>Customer </strong> {order.user.name}</li>
+                    <li class="list-group-item"><strong>Delivery Address </strong> {order.address}</li>
+                </ul>
+            </div>
+            ))
+    }
+
     return (
         <div>
         <Layout
@@ -30,7 +62,8 @@ function ListOrders() {
         >
             <div className="row">
                 <div className="col-md-6 mx-auto">
-                    { JSON.stringify(orders) }
+                    {notOrders()}
+                    { showOrders() }
                 </div>
             </div>
         </Layout>
